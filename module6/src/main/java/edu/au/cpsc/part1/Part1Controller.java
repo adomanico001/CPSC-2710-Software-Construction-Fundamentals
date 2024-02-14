7bc1c02ff762c54fa26599d18f2e0887c1f9bd28
Addie Domanico <adomanico001@gmail.com>
2024-02-14 16:26:15 -0500

    Update files

--- module6/src/main/java/edu/au/cpsc/part1/Part1Controller.java
+++ module6/src/main/java/edu/au/cpsc/part1/Part1Controller.java
@@ -33,9 +33,26 @@ public class Part1Controller {
 
   public void initialize() {
     // your bindings go here
+
+    // Create a property binding so that whatever is typed in messageTextField also shows in echoTextField
     echoTextField.textProperty().bind(messageTextField.textProperty());
+
+    /* Create a bidirectional property binding so that whatever is typed in firstBidirectionalTextField shows in
+       secondBidirectionalTextField and vise versa */
     firstBidirectionalTextField.textProperty().bindBidirectional(secondBidirectionalTextField.textProperty());
+
+    // Create a binding so that the opacity of the secretOverlapImageView is set by the value of the secretSlider
     secretOverlayImageView.opacityProperty().bind(secretSlider.valueProperty());
-    
+
+    // Create a binding so that the selectMeLabel will display true/false depending on the state of selectMeCheckBox
+    selectMeLabel.textProperty().bind(selectMeCheckBox.selectedProperty().asString());
+
+    // Create a binding so that numberOfCharactersLabel displays the number of letters in the tweetTextField
+    numberOfCharactersLabel.textProperty().bind(tweetTextField.textProperty().length().asString());
+
+    /* Create a binding so that validityLabel displays "Valid" if there are 10 characters or fewer in tweetTextField
+       and "Invalid" otherwise */
+    validityLabel.textProperty().bind(Bindings.when(tweetTextField.textProperty().length().lessThanOrEqualTo(10)).
+            then("Valid").otherwise("Invalid"));
   }
 }
