d629c192514c966d6b2e7e4f55042418dad00de8
Addie Domanico <adomanico001@gmail.com>
2024-02-18 15:18:15 -0500

    Updated module-info file

--- module6/src/main/java/module-info.java
+++ module6/src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module edu.au.cpsc.module6 {
+module edu.au.cpsc {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens edu.au.cpsc.module6 to javafx.fxml;
-    exports edu.au.cpsc.module6;
+    opens edu.au.cpsc.part1 to javafx.fxml;
+    exports edu.au.cpsc.part1;
+    opens edu.au.cpsc.part2 to javafx.fxml;
+    exports edu.au.cpsc.part2;
 }
